51c7bfe9d67bd44a9cf414114b93ec84ded0379a
RahulSondarva <Sondarvarahul8@gmail.com>
2025-07-03 11:58:04 +0530

    update code of login screen

--- composeApp/src/commonMain/kotlin/org/aifooddelivery/app/presentation/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/aifooddelivery/app/presentation/login/LoginScreen.kt
@@ -1,18 +1,16 @@
 package org.aifooddelivery.app.presentation.login
-
 import aifooddeliveryapp.composeapp.generated.resources.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -25,7 +23,7 @@ import org.aifooddelivery.app.presentation.componets.HeaderLargeTextStyle
 import org.aifooddelivery.app.presentation.componets.ReusableInputField
 import org.aifooddelivery.app.presentation.componets.ReusablePasswordField
 import org.aifooddelivery.app.presentation.home.navigation.MainScreen
-import org.aifooddelivery.app.presentation.login.viewModel.LoginResult
+import org.aifooddelivery.app.presentation.login.viewModel.LoginUiState
 import org.aifooddelivery.app.showToast
 import org.aifooddelivery.app.utils.DataStoreManager
 import org.jetbrains.compose.resources.painterResource
@@ -103,3 +101,162 @@ class LoginScreen : Screen {
     }
 }
 
+
+@Composable
+fun LoginScreenContent(
+    email: String,
+    password: String,
+    passwordVisible: Boolean,
+    emailError: String?,
+    passwordError: String?,
+    onEmailChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onTogglePasswordVisibility: () -> Unit,
+    onForgotPasswordClick: () -> Unit,
+    onLoginClick: () -> Unit,
+    onRegisterClick: () -> Unit,
+    isLoading: Boolean
+) {
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp, vertical = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                Spacer(Modifier.height(70.dp))
+                Text(
+                    text = stringResource(Res.string.login_to_your_account),
+                    lineHeight = 60.sp,
+                    style = HeaderLargeTextStyle,
+                )
+                Spacer(Modifier.height(15.dp))
+                Text(
+                    text = stringResource(Res.string.sign_in_your_account),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray
+                )
+                Spacer(Modifier.height(40.dp))
+
+                ReusableInputField(
+                    label = stringResource(Res.string.email_address),
+                    value = email,
+                    onValueChange = onEmailChange,
+                    placeholder = stringResource(Res.string.enter_email),
+                    isError = emailError != null,
+                    errorText = emailError,
+                    keyboardType = KeyboardType.Email
+                )
+
+                Spacer(Modifier.height(20.dp))
+
+                ReusablePasswordField(
+                    label = stringResource(Res.string.password),
+                    value = password,
+                    onValueChange = onPasswordChange,
+                    isVisible = passwordVisible,
+                    onVisibilityToggle = onTogglePasswordVisibility,
+                    isError = passwordError != null,
+                    errorText = passwordError
+                )
+
+                Spacer(Modifier.height(28.dp))
+                Text(
+                    text = stringResource(Res.string.forgot_password),
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .clickable { onForgotPasswordClick() },
+                    color = Color(0xFFFF9800),
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(Modifier.height(35.dp))
+                Button(
+                    onClick = onLoginClick,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(55.dp),
+                    shape = RoundedCornerShape(40.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF9800))
+                ) {
+                    Text(stringResource(Res.string.btn_sign_in), color = Color.White)
+                }
+
+                Spacer(Modifier.height(30.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Divider(modifier = Modifier.weight(1f))
+                    Text(
+                        text = stringResource(Res.string.or_sign_in_with),
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Divider(modifier = Modifier.weight(1f))
+                }
+
+                Spacer(Modifier.height(30.dp))
+                val icons =
+                    listOf(Res.drawable.ic_google, Res.drawable.ic_facebook, Res.drawable.ic_apple)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(
+                        16.dp,
+                        Alignment.CenterHorizontally
+                    )
+                ) {
+                    icons.forEach { iconRes ->
+                        Image(
+                            painter = painterResource(iconRes),
+                            contentDescription = null,
+                            modifier = Modifier.size(70.dp).padding(12.dp)
+                        )
+                    }
+                }
+            }
+
+            Column(
+                modifier = Modifier.fillMaxSize().padding(top = 35.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(stringResource(Res.string.dont_have_an_account))
+                    Spacer(Modifier.width(4.dp))
+                    Text(
+                        text = stringResource(Res.string.btn_register),
+                        color = Color(0xFFFF9800),
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.clickable { onRegisterClick() }
+                    )
+                }
+            }
+
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(24.dp),
+                    color = Color.White,
+                    strokeWidth = 2.dp
+                )
+            }
+        }
+
+        // Loader overlay
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0x80000000)) // semi-transparent black
+                    .wrapContentSize(Alignment.Center)
+            ) {
+                CircularProgressIndicator(
+                    color = Color(0xFFFF9800),
+                    strokeWidth = 3.dp,
+                    modifier = Modifier.size(50.dp)
+                )
+            }
+        }
+    }
+}
